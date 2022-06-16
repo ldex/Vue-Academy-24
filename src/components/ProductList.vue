2a77d57c06d52bd4de66498004374cea1e9f4371
ldex <laurent.duveau+git@gmail.com>
2022-06-16 13:33:39 -0400

    classe selected

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -3,7 +3,7 @@
         <h2>{{ title }}</h2>
         <ul class="products">
             <li v-for="product in products" v-bind:key="product.id"
-                v-bind:class='{ discontinued: product.discontinued }'
+                v-bind:class='{ discontinued: product.discontinued, selected: product === selectedProduct }'
                 @click="selectedProduct = product">
                 <span class="name">{{ product.name }}</span>
                 <span class="description">{{ product.description }}</span>
